d0e362555b5368c4d084c61dfae479686e6f7c12
ahsanmughal11 <sn092702@gmail.com>
2025-10-10 02:58:54 +0500

    number and array prop

--- SCFComponent.js
+++ SCFComponent.js
@@ -1,14 +1,27 @@
 
 export default {
- props: {
-  title: {
-    type: String,
-    required: true
-   },
-  content: {
-   type: String,
-   required: true
-   }
-  },
- template: '<h2>{{ title }}</h2><p>{{ content }}</p>'
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        content: {
+            type: String,
+            required: true
+        },
+        counting: {
+            type: Number,
+            required: false,
+        },
+        arrayprop: {
+            type: Array,
+            required: false,
+        }
+    },
+    template: `<h2>{{ title }}</h2>
+    <p>{{ content }}</p>
+    <p>{{ counting }}</p>
+    <ul>
+    <li v-for="item in arrayprop">{{item}}</li>
+    </ul>`
 }
