6c1889ab1884bdb9b15f2b4395e76e3649ff604c
ahsanmughal11 <sn092702@gmail.com>
2025-10-15 19:36:53 +0500

    component making in composition api style

---
+++ vite/VueVite/src/components/ReactiveComponent.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+let message = ref('');
+
+</script>
+<template>
+<div>
+    <label for="">Type the message:
+    <input type="text" v-model="message" />
+    </label>
+    <p>{{ message }}</p>
+    </div>
+</template>
+<style scoped>
+
+</style>
--- vite/VueVite/src/components/TheWelcome.vue
+++ vite/VueVite/src/components/TheWelcome.vue
@@ -5,11 +5,13 @@ import ToolingIcon from './icons/IconTooling.vue'
 import EcosystemIcon from './icons/IconEcosystem.vue'
 import CommunityIcon from './icons/IconCommunity.vue'
 import SupportIcon from './icons/IconSupport.vue'
+import ReactiveComponent from './ReactiveComponent.vue'
 
 const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
 </script>
 
 <template>
+<ReactiveComponent />
   <WelcomeItem>
     <template #icon>
       <DocumentationIcon />
