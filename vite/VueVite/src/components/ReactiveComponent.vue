9f897c3221e0110318f14e0374d52c04faf5f321
ahsanmughal11 <sn092702@gmail.com>
2025-10-15 19:57:32 +0500

    alert using sweetalert

--- vite/VueVite/src/components/ReactiveComponent.vue
+++ vite/VueVite/src/components/ReactiveComponent.vue
@@ -1,6 +1,14 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+import Swal from 'sweetalert'
 let message = ref('');
+onMounted(() => {
+    Swal({
+        title: 'Welcome!',
+        text: 'This is a reactive component using SweetAlert',
+        icon: 'success',
+    });
+});
 
 </script>
 <template>
